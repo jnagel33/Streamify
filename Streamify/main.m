fa754a50e49969bc140277fbb2f0727a2beb43f3
Josh Nagel <jnagel33@gmail.com>
2015-05-17 18:54:04 -0700

    Initial Commit

---
+++ Streamify/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Streamify
+//
+//  Created by Josh Nagel on 5/17/15.
+//  Copyright (c) 2015 jnagel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
